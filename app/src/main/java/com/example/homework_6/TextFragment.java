1f5dd5896e983000f0912f3ea4ea1fdb7cb8ea78
venom9954 <surzhikov99@gmail.com>
2022-07-29 13:50:26 +0300

    Дз. 8 В заголовок добавил аватарку и имя пользователя

--- app/src/main/java/com/example/homework_6/TextFragment.java
+++ app/src/main/java/com/example/homework_6/TextFragment.java
@@ -2,6 +2,8 @@ package com.example.homework_6;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
