971b02c060f899d16243409e64b5796357933312
venom9954 <surzhikov99@gmail.com>
2022-07-29 13:14:01 +0300

    Дз. 8 Создан обработчик кликов для элементов выдвижного меню. Создан новый фрагмент для отображения описания приложения

--- app/src/main/java/com/example/homework_6/MainActivity.java
+++ app/src/main/java/com/example/homework_6/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.homework_6;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -8,8 +9,12 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+
 import androidx.appcompat.widget.Toolbar;
 
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
@@ -36,6 +41,24 @@ public class MainActivity extends AppCompatActivity {
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, myToolbar, R.string.burger_open, R.string.burger_close );
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case (R.id.action_about):{
+                        getSupportFragmentManager().beginTransaction().replace(R.id.names, new AboutFragment()).addToBackStack("").commit();
+                        return true;
+                    }
+                    case (R.id.action_close):{
+                        finish();
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
     }
 
 
